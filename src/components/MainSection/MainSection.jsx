f1affe171078037747aea45af5e0cfb1744779dc
Семён Субочев <s.subochev@sz.ld>
2024-02-10 17:46:12 +0300

    MainSection success

--- src/components/MainSection/MainSection.jsx
+++ src/components/MainSection/MainSection.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
-import { ReactComponent as mainImage } from '../../images/girl.jpg';
-import { ReactComponent as logo1 } from '../../images/logo1.png';
-import { ReactComponent as logo2 } from '../../images/logo2.png';
-import { ReactComponent as logo3 } from '../../images/logo3.png';
-import { ReactComponent as logo4 } from '../../images/logo4.png';
+import MainImage from '../../images/girl.jpg';
+import { ReactComponent as Logo1 } from '../../images/1.svg';
+import { ReactComponent as Logo2 } from '../../images/2.svg';
+import { ReactComponent as Logo3 } from '../../images/3.svg';
+import { ReactComponent as Logo4 } from '../../images/4.svg';
 import Button from '../Button/Button';
 
 const MainSection = () => {
 	return (
-		<section className="w-full flex mt-60">
+		<section className="w-full flex mt-60 h-screen justify-between">
 			<div className="relative w-2/4 text-left mt-12">
 				<h1 className="text-8xl font-black mt-6 whitespace-pre-line">
 					{`Make\n remote work`}
@@ -19,6 +19,15 @@ const MainSection = () => {
 `}
 				</p>
 				<Button isFilled>Learn more</Button>
+				<div className="flex justify-around absolute bottom-2 w-full">
+					<Logo1 className="w-16"></Logo1>
+					<Logo2 className="w-16"></Logo2>
+					<Logo3 className="w-16"></Logo3>
+					<Logo4 className="w-16"></Logo4>
+				</div>
+			</div>
+			<div className="flex w-2/4 mt-20">
+				<img className="object-contain" src={MainImage} alt="main" />
 			</div>
 		</section>
 	);
